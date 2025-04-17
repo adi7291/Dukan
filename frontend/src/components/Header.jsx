22be7d33f6da8d253fc86653d6938487fb7580cd
Ramesh lal das <lalrameshdas@gmail.com>
2025-04-18 00:13:22 +0530

    removed some file

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,22 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-
+import logo from "../assets/logo.png";
 import React from "react";
 
 function Header() {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">DUKAN</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img  src={logo} alt="logo" />
+            DUKAN
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#cart">
-                <FaShoppingCart />
+                <FaShoppingCart /> Cart
               </Nav.Link>
               <Nav.Link href="#login">
                 <FaUser /> Sign In
